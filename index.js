9f64dad1149badc585e764df43ecd6eb89eca400
JulesRuby <JulesRuby@users.noreply.github.com>
2020-10-12 15:16:07 -0600

    Added scroll-hide for the mobile menu... Forget what else? Oh, playing with form input animations, but that's for later mostly

--- index.js
+++ index.js
@@ -44,3 +44,43 @@ function handleActive() {
 }
 
 menuLinks.forEach(link => link.addEventListener('click', handleActive));
+
+// ----MOBILE NAV ONSCROLL WITHDRAW
+// Select the nav elements to shift
+const mobileMenu = document.querySelector('.navbar');
+const contactBar = document.querySelector('.contact-strip');
+let lastScrollPosition = window.pageYOffset;
+// create window scroll event listener4
+const menuHide = () => {
+	window.addEventListener('scroll', () => {
+		const currentScrollPosition = window.pageYOffset;
+		const scrollDistance = currentScrollPosition - lastScrollPosition;
+		// console.log('current: ' + currentScrollPosition);
+		// console.log('last: ' + lastScrollPosition);
+		// console.log('difference: ' + scrollDistance);
+		// console.log('difference: ' + parseFloat(scrollDistance));
+		// deal with inertia scroll
+		if (currentScrollPosition < 0 || (scrollDistance < 200 && scrollDistance > -200)) {
+			return;
+		}
+
+		if( scrollDistance >= 200 ) {
+			mobileMenu.classList.add('scroll-hide');
+			contactBar.classList.add('scroll-hide');
+			lastScrollPosition = currentScrollPosition;
+		}
+		// debugger;
+		if ( parseFloat(scrollDistance) <= (-200) ) {
+			console.log('WHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE');
+			mobileMenu.classList.remove('scroll-hide');
+			contactBar.classList.remove('scroll-hide');
+			lastScrollPosition = currentScrollPosition;
+		}
+
+	})
+}
+
+menuHide();
+
+// onscroll x distance down the page, retract menu
+// onscroll x distance up the page, expand menu
