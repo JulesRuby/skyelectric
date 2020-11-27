350430a9e99a00aa72aaa6ff1f0e822084e819fa
JulesRuby <JulesRuby@users.noreply.github.com>
2020-11-27 15:47:19 -0700

    changed logo to an svg so I can more easily manipulate it for the navbar. Removed junk code from JS file and re-wrote code to actually make the active links work as intended.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const vport = document.querySelector('.vport');
+// const vport = document.querySelector('.vport');
 
-if (vport) {
-	window.addEventListener(('resize'), () => {
-		let output = window.innerWidth;
-		console.log(output);
-		console.log(vport);
+// if (vport) {
+// 	window.addEventListener(('resize'), () => {
+// 		let output = window.innerWidth;
+// 		console.log(output);
+// 		console.log(vport);
 		
-		return vport.innerText = output;
-	})
-} else {
-	console.log('No .vport classed element present, uncomment it in the HTML if you want to have a live output of the window.innerWidth');
-}
+// 		return vport.innerText = output;
+// 	})
+// } else {
+// 	console.log('No .vport classed element present, uncomment it in the HTML if you want to have a live output of the window.innerWidth');
+// }
 
 // ----MOBILE MENU EXPAND
 const toggleMenu = (hamburgerEl, menuEl) => {
@@ -30,20 +30,45 @@ toggleMenu('.hamburger', '.nav-menu');
 // ----ACTIVE LINKS
 // Let's try setting the active link on the page when you click the menu
 
-const menuLinks = document.querySelectorAll('.navlink');
+// So that was basically only good for baby stuff, fuck off, babies. Nobody wants you.
 
-function handleActive() {
-	menuLinks.forEach(link => link.classList.remove('active-link'));
-	console.log(menuLinks)
-	this.classList.add('active-link');
+// const menuLinks = document.querySelectorAll('.navlink');
 
-	const hamburger = document.querySelector('.hamburger');
-	const menu = document.querySelector('.nav-menu');
-	hamburger.classList.remove('open');
-	menu.classList.remove('expanded');
+// function handleActive() {
+// 	menuLinks.forEach(link => link.classList.remove('active-link'));
+// 	console.log(menuLinks)
+// 	this.classList.add('active-link');
+
+// 	const hamburger = document.querySelector('.hamburger');
+// 	const menu = document.querySelector('.nav-menu');
+// 	hamburger.classList.remove('open');
+// 	menu.classList.remove('expanded');
+// }
+
+// menuLinks.forEach(link => link.addEventListener('click', handleActive));
+
+//  THE REEEEALLLLL ACTIVE NAV SHIZ YEYEYEYEYEE
+
+// trying to decide if it's better to just store the navLinks in global scope, or retrive and shed them with a nuew function execution on each load? Look more into this, or if this level of optimization even matter for my piddly code.
+
+// const navLinks = document.querySelectorAll('.navlink');
+// console.log(navLinks);
+const navLinks = document.querySelectorAll('.navlink');
+const chooseActive = navLinks => {
+	// const navLinks = document.querySelectorAll('.navlink');
+	// get the current url pathway
+	const currentPath = document.location.pathname;
+	// remove active class from all links
+	navLinks.forEach(link => {
+		link.classList.remove('active-link');
+		if (link.pathname === currentPath) {
+			link.classList.add('active-link');
+		}
+	});
+
 }
 
-menuLinks.forEach(link => link.addEventListener('click', handleActive));
+chooseActive(navLinks);
 
 // ----MOBILE NAV ONSCROLL WITHDRAW
 // Select the nav elements to shift
@@ -71,7 +96,7 @@ const menuHide = () => {
 		}
 		// debugger;
 		if ( parseFloat(scrollDistance) <= (-200) ) {
-			console.log('WHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE');
+			// console.log('WHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE');
 			mobileMenu.classList.remove('scroll-hide');
 			contactBar.classList.remove('scroll-hide');
 			lastScrollPosition = currentScrollPosition;
@@ -85,26 +110,26 @@ menuHide();
 
 
 
-const setPulls = elements => {
-  const elementList = document.querySelectorAll(elements);
-  console.log(elementList);
+// const setPulls = elements => {
+//   const elementList = document.querySelectorAll(elements);
+//   // console.log(elementList);
   
-  elementList.forEach(element => {
-    const button = element.querySelector('.service-expand');
-    const pullDown = element.querySelector('.pulldown');
-    const pdText = element.querySelector('.p');
-    const pdUl = element.querySelector('.ul');
+//   elementList.forEach(element => {
+//     const button = element.querySelector('.service-expand');
+//     const pullDown = element.querySelector('.pulldown');
+//     const pdText = element.querySelector('.p');
+//     const pdUl = element.querySelector('.ul');
     
-    // button.addEventListener("click", handlePullDown(button, pullDown, pdText, pdUl));
-    button.addEventListener("click", () => {
-      //  The blur just prevents the button from maintaining focus when clicked. I only want focus to remain if it's being tabbed to.                            
-      button.blur();
-      button.classList.toggle('pull-open');
-      pullDown.classList.toggle('pull-open');
-      pdText.classList.toggle('pull-open');
-      pdUl.classList.toggle('pull-open');
-      })
-  })
-}
+//     // button.addEventListener("click", handlePullDown(button, pullDown, pdText, pdUl));
+//     button.addEventListener("click", () => {
+//       //  The blur just prevents the button from maintaining focus when clicked. I only want focus to remain if it's being tabbed to.                            
+//       button.blur();
+//       button.classList.toggle('pull-open');
+//       pullDown.classList.toggle('pull-open');
+//       pdText.classList.toggle('pull-open');
+//       pdUl.classList.toggle('pull-open');
+//       })
+//   })
+// }
 
-setPulls('.service-container');
+// setPulls('.service-container');
